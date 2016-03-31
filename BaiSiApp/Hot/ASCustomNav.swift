f532c4a58525966929e96e85f665bf0ee7ebffe1
Ashen-Zhao <zhaoashen@gmail.com>
2016-03-31 18:04:02 +0800

    动态导航

--- BaiSiApp/Hot/ASCustomNav.swift
+++ BaiSiApp/Hot/ASCustomNav.swift
@@ -8,22 +8,26 @@
 
 import UIKit
 
-class ASCustomNav: UIView {
+class ASCustomNav: UIView, UIScrollViewDelegate {
     
     
     var scrollView:UIScrollView!
     var leftImg:UIImageView!
     var rightImg:UIImageView!
     var randowBtn:UIButton!
+    var lineView:UIView!
+    var lastBtn:UIButton?
     
     override init(frame: CGRect) {
         super.init(frame: frame)
-        makeUI()
+        
         ASDataHelper.getMenusType("精华") { (AnyObject) in
             let array = AnyObject as! NSMutableArray
+            let tempArr = NSMutableArray()
             for model in array {
-                print((model as! ASMenusModel).name)
+                tempArr.addObject((model as! ASMenusModel).name)
             }
+            self.makeUI(tempArr)
         }
     }
     
@@ -31,11 +35,14 @@ class ASCustomNav: UIView {
         fatalError("init(coder:) has not been implemented")
     }
     
-    func makeUI() {
+    
+    // MARK: - Methods
+    func makeUI(titles:NSMutableArray) {
         
-        scrollView = UIScrollView(frame: CGRect.init(x: 0, y: 0, width:self.frame.size.width - 50, height: self.frame.size.height))
+        scrollView = UIScrollView(frame: CGRect.init(x: 0, y: 0, width:self.frame.size.width - 35, height: self.frame.size.height))
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.showsVerticalScrollIndicator = false
+        scrollView.delegate = self
         self.addSubview(scrollView)
         
         leftImg = UIImageView(frame: CGRect.init(x: 0, y: 0, width: 10, height: scrollView.frame.size.height))
@@ -51,23 +58,72 @@ class ASCustomNav: UIView {
         randowBtn.setImage(UIImage(named: "RandomAcross"), forState: .Normal)
         self.addSubview(randowBtn)
         
-        scrollView.contentSize = CGSize(width: 9 * 50, height: self.frame.size.height)
-        for i in 0...8 {
+        scrollView.contentSize = CGSize(width: CGFloat(titles.count * 50), height: self.frame.size.height)
+        for i in 0..<titles.count {
             let title = UIButton(type: .Custom)
             title.frame = CGRectMake(CGFloat(i * 50), 0, 50, self.frame.size.height)
-            title.setTitle("视频", forState: UIControlState.Normal)
-            title.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
-            title.titleLabel?.font = UIFont.systemFontOfSize(15)
+            title.setTitle(titles[i] as? String, forState: UIControlState.Normal)
+            title.tag = i;
+            if i == 0 {
+                lastBtn = title
+                title.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+                title.titleLabel?.font = UIFont.systemFontOfSize(17)
+            } else {
+                title.setTitleColor(UIColor.lightGrayColor(), forState: UIControlState.Normal)
+                title.titleLabel?.font = UIFont.systemFontOfSize(15)
+            }
+            title.addTarget(self, action: #selector(ASCustomNav.titlesChangeAction(_:)), forControlEvents: .TouchUpInside)
             scrollView.addSubview(title)
+        }
+        
+        lineView = UIView(frame: CGRectMake(9, CGRectGetMaxY(scrollView.frame) - 3, 30, 2))
+        lineView.backgroundColor = UIColor.redColor()
+        scrollView.addSubview(lineView)
+    }
+    
+    
+    // MARK: - delegate
+    
+    func scrollViewDidScroll(scrollView: UIScrollView) {
+        print("\(scrollView.contentSize.width) \n \(scrollView.contentOffset.x)")
+    }
+    
+    // MARK: - actons
+    func titlesChangeAction(btn:UIButton) {
+        
+        
+        if lastBtn?.tag == btn.tag {
+            return
+        }
+        
+        lastBtn?.setTitleColor(UIColor.lightGrayColor(), forState: .Normal)
+        lastBtn?.titleLabel?.font = UIFont.systemFontOfSize(15)
+        
+        btn.setTitleColor(UIColor.redColor(), forState: UIControlState.Normal)
+        btn.titleLabel?.font = UIFont.systemFontOfSize(17)
+        
+        lastBtn = btn
+        
+        var frame = lineView.frame
+        frame.origin.x = CGFloat(btn.tag * 50) + 9
+    
+        var dict = [Int:CGFloat]()
+       
+        dict = [0:0.0, 1:0.0, 2:0.0, 3:50.0, 4:100.0, 5:150.0, 6:160.0, 7:180.0, 8:180.0]
+        
+        UIView.animateWithDuration(0.6) {
+            self.scrollView.contentOffset.x = dict[btn.tag]!
+            self.lineView.frame = frame
         }
     }
     
+    
     /*
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-    }
-    */
-
+     // Only override drawRect: if you perform custom drawing.
+     // An empty implementation adversely affects performance during animation.
+     override func drawRect(rect: CGRect) {
+     // Drawing code
+     }
+     */
+    
 }
